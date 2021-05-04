867a1a7765ebe09b64c0fc7668477c95544bc0c5
Osumi Akari <kig.osumiakari@gmail.com>
2021-05-04 22:34:17 +0900

    add error page

---
+++ components/millermenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar is-black" role="navigation">
+    <div class="navbar-brand">
+      <nuxt-link to="/">
+        nuxtblog-miller
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<style>
+@import "node_modules\bulma\css\bulma.css";
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+/* eslint-disable vue/no-multiple-template-root */
+<template>
+  <h1 class="title is-centered">
+    Error code: {{ error.statusCode }} / Message: {{ error.message }}
+  </h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['error']
+}
+</script>
+
+<style>
+@import "node_modules\bulma\css\bulma.css";
+</style>
