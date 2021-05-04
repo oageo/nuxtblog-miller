565f1a563857402d5253b66ace26a34093e21fae
Osumi Akari <kig.osumiakari@gmail.com>
2021-05-04 22:13:52 +0900

    add _slug.vue & index.vue

---
+++ content/articles/test.md
@@ -0,0 +1,7 @@
+---
+title: Test title
+description: Test description
+date: 2021-05-04
+---
+
+## Test h2
---
+++ pages/articles/_slug.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="is-dark">
+    <div class="hero-body is-brack">
+      <h1 class="title">
+        {{ articles.title }}
+      </h1>
+      <p class="subtitle">
+        {{ articles.description }} / {{ articles.date }}
+      </p>
+    </div>
+    <nuxt-content :document="articles" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const articles = await $content('articles', params.slug).fetch()
+    return { articles }
+  }
+}
+</script>
+
+<style>
+@import "node_modules\bulma\css\bulma.css";
+</style>
