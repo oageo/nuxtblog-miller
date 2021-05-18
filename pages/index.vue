26beaaff41dce6c51728dc6d98877bdacc8b8165
Osumi Akari <kig.osumiakari@gmail.com>
2021-05-18 22:47:26 +0900

    fix display order of articles

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,14 @@
   <div>
     <div v-for="a in articles" :key="a.date" class="m-4">
       <nuxt-link :to="'/articles/'+ a.slug">
-        <h1 class="title">
-          {{ a.title }}
-        </h1>
-        {{ a.date }}
+        <div>
+          <h1 class="title">
+            {{ a.title }}
+          </h1>
+          <p class="subtitle">
+            {{ a.date }}
+          </p>
+        </div>
       </nuxt-link>
     </div>
   </div>
@@ -14,7 +18,7 @@
 <script>
 export default {
   async asyncData ({ $content, params }) {
-    const query = await $content('articles' || 'index').limit(10)
+    const query = await $content('articles' || 'index').sortBy('createdAt', 'desc').limit(10)
     const articles = await query.fetch()
     return { articles }
   },
