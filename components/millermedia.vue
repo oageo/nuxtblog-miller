f762c51b6850629ad45001239d5dd36253f29dcd
Osumi Akari <kig.osumiakari@gmail.com>
2022-03-22 18:51:36 +0900

    add millermedia.vue

---
+++ components/millermedia.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="millermedia">
+    <!-- ビデオ用 -->
+    <div v-if="vsource != ''" class="millervideo">
+      <video class="player">
+        <source src="{{ vsource }}">
+      </video>
+    </div>
+    <!-- オーディオ用 -->
+    <div v-else-if="asource != ''" class="milleraudio">
+      <video class="player">
+        <source src="{{ asource }}">
+      </video>
+    </div>
+    <!-- YouTube用 -->
+    <div v-else-if="ytvid != ''" class="milleryt">
+      <div class="player">
+        <div data-video-id="{{ ytvid }}" data-type="youtube" />
+      </div>
+    </div>
+    <!-- Vimeo用 -->
+    <div v-else-if="vimeovid != ''" class="millervimeo">
+      <div class="player" data-plyr-provider="vimeo" data-plyr-embed-id="{{ vimeovid }}" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { plyr } from 'plyr'
+const player = new plyr('.player')
+
+export default ({
+  props: [
+    'vsource',
+    'asource',
+    'ytvid',
+    'vimeovid'
+  ]
+})
+</script>
+
+<style scoped>
+@import url(https://cdnjs.cloudflare.com/ajax/libs/plyr/3.6.12/plyr.min.css);
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,17 @@
   </div>
 </template>
 
+<script>
+import millermedia from './millermedia.vue'
+
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    millermedia
+  }
+}
+</script>
+
 <style>
 @import "node_modules\bulma\css\bulma.min.css";
 </style>
