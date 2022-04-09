2c9a6b03aa35556a09a33f62fcad56eefc5cc254
Osumi Akari <kig.osumiakari@gmail.com>
2022-04-09 22:05:20 +0900

    test make milleryt.vue

---
+++ components/milleryt.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="videoyt">
+      <iframe :src="'https://youtube-nocookie.com/embed/' + ytid" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loading=lazy allowfullscreen>
+        milleryt.vue: something broken
+      </iframe>
+  </div>
+</template>
+
+<style>
+@import "node_modules\bulma\css\bulma.min.css";
+</style>
+
+<script>
+export default {
+    props: [ytvid]
+}
+</script>
