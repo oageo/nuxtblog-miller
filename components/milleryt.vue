dbfae6607f8a3dc1051bf66a9e9bbbb9dc330df1
Osumi Akari <kig.osumiakari@gmail.com>
2022-04-10 12:36:32 +0900

    chenge error messeage in millertyt.vue

--- components/milleryt.vue
+++ components/milleryt.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="videoyt milleryt">
       <iframe :src="'https://youtube.com/embed/' + ytvid" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loading=lazy>
-        milleryt.vue: something broken
+        milleryt.vue: something broken.
+        <ul>
+          <li>
+            ytvid: {{ ytvid }}
+          </li>
+          <li>
+            Please access to <a :href="'https://www.youtube.com/watch?=' + ytvid">youtube.com</a>
+          </li>
+        </ul>
       </iframe>
   </div>
 </template>
