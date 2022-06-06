632bf8870d27d52cf5ce08df33fea881f5e6eafa
Osumi Akari <kig.osumiakari@gmail.com>
2022-06-07 00:23:55 +0900

    changed OGP

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,21 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'This description can be changed at nuxt.config.js' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'This description can be changed at nuxt.config.js'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: this.title
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: this.description
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
