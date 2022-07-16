d6d37cd936598411a3c63bfd8a176b2218cd17ab
Osumi Akari <kig.osumiakari@gmail.com>
2022-07-16 19:16:22 +0900

    add when no license message in articles

--- components/millerlicense.vue
+++ components/millerlicense.vue
@@ -20,6 +20,10 @@
     <div v-else-if="license === 'ccbysa40'" class="ccby40">
       <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a><br>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
     </div>
+    <!-- No licesed -->
+    <div>
+      <p>This articles has no license for your use. Please contact me when you want to use this article's materials.</p>
+    </div>
   </div>
 </template>
 
