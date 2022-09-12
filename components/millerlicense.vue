d26c01c6c9279c561792370ae1aa631122d00ed3
Osumi Akari <kig.osumiakari@gmail.com>
2022-09-13 01:13:20 +0900

    fix miller license

--- components/millerlicense.vue
+++ components/millerlicense.vue
@@ -21,7 +21,7 @@
       <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a><br>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
     </div>
     <!-- No licesed -->
-    <div>
+    <div v-else class="none">
       <p>This articles has no license for your use. Please contact me when you want to use this article's materials.</p>
     </div>
   </div>
