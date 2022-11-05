6c539b66f335ad5aa311fe090a4497ca536ce0e9
Osumi Akari <kig.osumiakari@gmail.com>
2022-11-05 09:21:15 +0900

    add loding lazy at millerlicense

--- components/millerlicense.vue
+++ components/millerlicense.vue
@@ -4,7 +4,7 @@
     <div v-if="license === 'cc010'" class="cc010">
       <p xmlns:dct="http://purl.org/dc/terms/">
         <a rel="license" href="http://creativecommons.org/publicdomain/zero/1.0/">
-          <img src="http://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0">
+          <img loading="lazy" src="http://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0">
         </a>
         <br>
         To the extent possible under law,
@@ -14,11 +14,11 @@
     </div>
     <!-- CC BY 4.0 -->
     <div v-else-if="license === 'ccby40'" class="ccby40">
-      <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png"></a><br>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+      <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img loading="lazy" alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png"></a><br>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
     </div>
     <!-- CC BY-SA 4.0 -->
     <div v-else-if="license === 'ccbysa40'" class="ccby40">
-      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a><br>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img loading="lazy" alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a><br>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
     </div>
     <!-- No licesed -->
     <div v-else class="none">
