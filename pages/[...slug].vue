4aa3d9b6e6a626d05bebe1b2189fa8680ef9950b
Osumi Akari <kig.osumiakari@gmail.com>
2022-11-08 14:40:15 +0900

    look https://github.com/nuxt/content/issues/1287

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,10 @@
 <template>
   <main>
-    <ContentDoc :path="$route.path" />
+    <ContentDoc :path="$route.path">
+      <template v-slot="{ articles }">  
+        <h1>{{ articles.title }}</h1>
+        <ContentRenderer :value="articles" />
+      </template>
+    </ContentDoc>
   </main>
 </template>
--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -1,5 +1,10 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc :path="$route.path">
+      <template v-slot="{ articles }">  
+        <h1>{{ articles.title }}</h1>
+        <ContentRenderer :value="articles" />
+      </template>
+    </ContentDoc>
   </main>
 </template>
