b422c9e3b73d4e1f293186079025ad995ad6adc8
Osumi Akari <kig.osumiakari@gmail.com>
2022-11-08 14:29:18 +0900

    test route [..slug].vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,6 @@
 import type { QueryBuilderParams } from "@nuxt/content/dist/runtime/types";
 const query: QueryBuilderParams = {
   path: "/articles",
-  limit: 5,
-  sort: { date: -1 },
+  limit: 5
 };
 </script>
