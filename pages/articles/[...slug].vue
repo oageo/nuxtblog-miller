5b1287387af12fd4a9a32f7b7b96ba497289457f
Osumi Akari <kig.osumiakari@gmail.com>
2022-11-08 14:52:33 +0900

    remake  simple <ContentDoc /> pages

--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -1,10 +1,5 @@
 <template>
   <main>
-    <ContentDoc :path="$route.path">
-      <template v-slot="{ articles }">  
-        <h1>{{ articles.title }}</h1>
-        <ContentRenderer :value="articles" />
-      </template>
-    </ContentDoc>
+    <ContentDoc />
   </main>
 </template>
