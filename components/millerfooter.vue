38e5f1f1818e344f85db75a2afdd6c50d5c2b7a6
Osumi Akari <kig.osumiakari@gmail.com>
2022-11-11 22:58:19 +0900

    add millermenu&millerfooter

---
+++ components/millerfooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer">
+    <p>This is "millerfooter".</p>
+    <p>You can edit this footer on "components/millerfooter.vue".</p>
+  </footer>
+</template>
+
+<style>
+</style>
