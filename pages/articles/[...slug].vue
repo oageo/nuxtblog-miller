df3fbf1d1b6518427b30cbde4d995471d1d7f476
Osumi Akari <kig.osumiakari@gmail.com>
2022-11-15 23:47:34 +0900

    add columns

--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -1,7 +1,12 @@
 <template>
-  <main>
-    <div class="content">
-      <ContentDoc />
+  <main class="columns is-centered is-flex-wrap-wrap">
+    <div class="column is-8 m-4">
+      <div class="content">
+        <ContentDoc />
+      </div>
+    </div>
+    <div class="column is-4 m-2">
+
     </div>
   </main>
 </template>
