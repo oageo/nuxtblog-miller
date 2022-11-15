626cd32eb42c4aacf47425b22cb623e5f64a4624
Osumi Akari <kig.osumiakari@gmail.com>
2022-11-16 00:02:57 +0900

    change to dark

--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -1,12 +1,20 @@
 <template>
-  <main class="columns is-centered is-flex-wrap-wrap">
-    <div class="column is-8 m-4">
-      <div class="content">
-        <ContentDoc />
+  <div class="has-background-grey-darker has-background-white">
+    <main class="columns is-centered is-flex-wrap-wrap">
+      <div class="column is-8 m-4">
+        <div class="content">
+          <ContentDoc />
+        </div>
       </div>
-    </div>
-    <div class="column is-4 m-2">
+      <div class="column is-4 m-2">
 
-    </div>
-  </main>
+      </div>
+    </main>
+  </div>
 </template>
+
+<style>
+.nuxt-content a {
+color: hsl(217, 71%, 60%)
+}
+</style>
